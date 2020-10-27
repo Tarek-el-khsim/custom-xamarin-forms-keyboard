fff5ceb5da6de9e121233d47f307ac2cdfb02688
Tarek EL KHSIM <tarek.el.khsim@gmail.com>
2020-10-27 20:54:39 +0100

    Change keyboard type manually

---
+++ src/CustomKeyboard.iOS/Renderers/EntryWithCustomKeyboardRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using CustomKeyboard;
+using CustomKeyboard.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EntryWithCustomKeyboard), typeof(EntryWithCustomKeyboardRenderer))]
+
+namespace CustomKeyboard.iOS.Renderers
+{
+    class EntryWithCustomKeyboardRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> args)
+        {
+            base.OnElementChanged(args);
+            this.Control.KeyboardType = UIKeyboardType.NumberPad;
+
+
+
+            UIToolbar toolBar = new UIToolbar(new RectangleF(0.0f, 0.0f, (float)this.Frame.Size.Width, 44.0f));
+            toolBar.Translucent = true;
+            toolBar.Items = new UIBarButtonItem[] {
+                new UIBarButtonItem("123", UIBarButtonItemStyle.Bordered, delegate {
+                    this.Control.KeyboardType = UIKeyboardType.NumberPad;
+                    this.Control.ResignFirstResponder();
+
+                }),
+                new UIBarButtonItem("ABC", UIBarButtonItemStyle.Bordered, delegate {
+                    this.Control.KeyboardType = UIKeyboardType.Default;
+                    this.Control.ResignFirstResponder();
+                }),
+                new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+                new UIBarButtonItem (UIBarButtonSystemItem.Done, delegate {
+                    this.Control.ResignFirstResponder();
+                })
+            };
+
+            this.Control.InputAccessoryView = toolBar;
+
+        }
+    }
+}
